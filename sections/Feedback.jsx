e73bbad9fc6b216da6b31808ab63eea309ba5925
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2023-08-31 01:07:58 +0300

    remove

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,23 +1,13 @@
 "use client";
 
-import { motion } from "framer-motion";
-
 import styles from "../styles";
-import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
 
 const Feedback = () => (
   <section className={`${styles.paddings}`}>
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
+    <div
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
     >
-      <motion.div
-        variants={fadeIn("right", "tween", 0.1, 0.2)}
-        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
-      >
+      <div className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative">
         <div className="feedback-gradient" />
         <div>
           <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
@@ -33,30 +23,24 @@ const Feedback = () => (
           for today's work, or can be called web 3.0. by using metaverse you can
           use it as anything”
         </p>
-      </motion.div>
+      </div>
 
-      <motion.div
-        variants={fadeIn("left", "tween", 0.2, 1)}
-        className="relative flex-1 flex justify-center items-center"
-      >
+      <div className="relative flex-1 flex justify-center items-center">
         <img
           src="/planet-09.png"
           alt="planet-09"
           className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
         />
 
-        <motion.div
-          variants={zoomIn(0.1, 1)}
-          className="lg:block hidden absolute -left-[10%] top-[3%]"
-        >
+        <div className="lg:block hidden absolute -left-[10%] top-[3%]">
           <img
             src="/stamp.png"
             alt="stamp"
             className="w-[155px] h-[155px] object-contain"
           />
-        </motion.div>
-      </motion.div>
-    </motion.div>
+        </div>
+      </div>
+    </div>
   </section>
 );
 
